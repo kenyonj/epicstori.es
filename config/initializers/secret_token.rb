586748cc612ad47697f139f01758f10ce880db59
Justin Kenyon <kenyonj@gmail.com>
2014-05-12 14:38:36 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Epicstories::Application.config.secret_key_base = '51fd71c6eb8b17e25b1a93371cb0f1284bc67da20c8e5166f46adf96f035f1d277e52313d9e7a90f93ecb233e867872aea0241c16b74814745077ba8270f8987'
