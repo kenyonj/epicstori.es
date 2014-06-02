3b692fa5958dd9e2ab454f82e212867585d39a90
Justin Kenyon <kenyonj@gmail.com>
2014-06-02 16:53:37 -0400

    Adds user_signs_up feature spec

--- spec/features/user_signs_in_spec.rb
+++ spec/features/user_signs_in_spec.rb
@@ -1,30 +1,60 @@
-require 'spec_helper'
+require "spec_helper"
 
-feature 'User signs in' do
-  scenario 'with email address and correct password' do
-    create(:user, username: 'justin', email: 'test@test.com', password_digest: 'password')
+feature "User signs in" do
+  scenario "with email address and correct password" do
+    create_user
     visit root_path
 
-    within '#new_session' do
-      fill_in 'Email or username', with: 'test@test.com'
-      fill_in 'Password', with: 'password'
-    end
-    click_button 'Sign in'
+    login_with(login: "test@test.com", password: "password")
 
     expect(page).to have_content "Welcome justin"
     expect(page).to have_content "Sign Out"
   end
 
-  scenario 'with email address and incorrect password' do
+  scenario "with email address and incorrect password" do
+    create_user
     visit root_path
 
-    within '#new_session' do
-      fill_in 'Email or username', with: 'foo@bar.com'
-      fill_in 'Password', with: '12345'
-    end
+    login_with(login: "foo@bar.com", password: "12345")
 
-    click_button 'Sign in'
     expect(page).to have_content "Email"
     expect(page).to have_content "Password"
+  end
+
+  scenario "with username and correct password" do
+    create_user
+    visit root_path
+
+    login_with(login: "justin", password: "password")
+
+    expect(page).to have_content "Welcome justin"
+    expect(page).to have_content "Sign Out"
+  end
+
+  scenario "with username and incorrect password" do
+    create_user
+    visit root_path
+
+    login_with(login: "justin", password: "12345")
+
+    expect(page).to have_content "Email"
+    expect(page).to have_content "Password"
+  end
+
+  def create_user
+    create(
+      :user,
+      username: "justin",
+      email: "test@test.com",
+      password_digest: "password"
+    )
+  end
+
+  def login_with(options = {})
+    within "#new_session" do
+      fill_in "Email or username", with: options[:login]
+      fill_in "Password", with: options[:password]
+    end
+    click_button "Sign in"
   end
 end
